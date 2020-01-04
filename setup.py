2a23aa8f3a4bbe57567311c373741697c1214a1f
zommiommy <zommy21@hotmail.it>
2020-01-04 15:15:25 +0100

    fixed folder name FFS

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="genome_windows_generator", # Replace with your own username
+    name="windows_generator", # Replace with your own username
     version="0.0.1",
     author="Tommaso Fontana",
     author_email="tommaso.fontana.96@gmail.com",
     description="Generator of nucleotides windwos from a given assembly",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/zommiommy/genome_windows_generator",
+    url="https://github.com/zommiommy/windows_generator",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
